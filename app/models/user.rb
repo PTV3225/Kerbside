baf32ac9fb7cf3f4a3653eb534a674061b61dff0
Shiva Muraleedaran <shivamuraleedaran@hotmail.com>
2023-08-10 22:01:00 +1000

    models and schemas generated, still need to ammend to include photos according to photo hosting lecture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
+
+  has_many :messages
+  has_many :posts
+
+
+  #might need to add first name and last name into the db for users and also photo for avatar
+
+
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
 end
