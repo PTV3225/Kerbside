d38ba96a866fe7beefd8ea5500b8ac9a1a850984
Shiva Muraleedaran <shivamuraleedaran@hotmail.com>
2023-08-19 16:24:46 +1000

    chatroom functionality ammended

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,11 @@ Rails.application.routes.draw do
 
   resources :posts do
     resources :treasures, only: [:new, :create]
-
-  end
-
-
-  resources :chatrooms, only: :show do
+    resources :chatrooms, only: :show do
       resources :messages, only: [:create, :destroy]
     end
+  end
+
 
 
   # Defines the root path route ("/")
