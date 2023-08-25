c9d91bcdebc21c3fa2b1412e3a9c396328151795
Alena Andreichuk <andreychuk.lena@gmail.com>
2023-08-25 22:37:23 +0200

    Favoriting posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,18 @@ class PostsController < ApplicationController
     end
   end
 
+  def favorite
+    @post = Post.find(params[:id])
+    current_user.favorite(@post)
+    redirect_to @post, notice: 'Post favorited!'
+  end
+
+  def unfavorite
+    @post = Post.find(params[:id])
+    current_user.unfavorite(@post)
+    redirect_to @post, notice: 'Post unfavorited!'
+  end
+
   def new
     @post = Post.new
   end
@@ -44,18 +56,6 @@ class PostsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
-  end
-
-  def favorite
-    @post = Post.find(params[:id])
-    current_user.favorite(@post)
-    redirect_to @post, notice: 'Post favorited!'
-  end
-
-  def unfavorite
-    @post = Post.find(params[:id])
-    current_user.unfavorite(@post)
-    redirect_to @post, notice: 'Post unfavorited!'
   end
 
   def destroy
