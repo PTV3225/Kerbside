2b8a2c639428d4deb48192bb76d4b2e76cbea1f3
Shiva Muraleedaran <121623614+shivamuraleedaran@users.noreply.github.com>
2023-09-09 11:51:06 +1000

    timezone set

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,7 +22,7 @@ module Kerbside
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Sydney"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
