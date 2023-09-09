cfcbe2515686d8fbad4570efa60e5a7f40ca6409
Peter Varley <128195593+PTV3225@users.noreply.github.com>
2023-09-09 10:20:09 +1000

    minor front end

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -5,8 +5,10 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["the treasure in your trash"],
-      typeSpeed: 50,
+      strings: ["the treasure in your trash", "a bear in the there", "the guitar for your hero", "a feather for your cap"],
+      typeSpeed: 40,
+      backSpeed: 50, // Optional: Speed of backspacing, you can adjust as needed
+      backDelay: 1500,
       loop: true,
       showCursor: false,
     })
